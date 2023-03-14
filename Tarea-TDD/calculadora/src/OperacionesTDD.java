f2cbefee35fcd1e75fcd45b2d9f45cd3325cd9c8
Adrynb <adriannavarrobuceta@gmail.com>
2023-03-14 18:18:35 +0100

    Calculadora básica (Devuelve 0 y 1)

--- Tarea-TDD/calculadora/src/OperacionesTDD.java
+++ Tarea-TDD/calculadora/src/OperacionesTDD.java
@@ -3,28 +3,6 @@ import java.util.Arrays;
 
 public class OperacionesTDD {
 
-    public String calc (String operando){
-        if(operando.equals("")){
-            return "0";
-        }
-        else{
-            return operando;
-        }
-    }
 
-    public int calc (int suma1, int suma2){
-        if(suma1 < 0 || suma2 < 0){
-
-        }
-        return suma1 + suma2;
-
-    }
-
-    public int calc2(int suma1, int suma2, int suma3){
-        if(suma1 < 0 || suma2 < 0 || suma3 < 0){
-
-        }
-        return suma1 + suma2 + suma3;
-    }
 
 }
--- Tarea-TDD/calculadora/src/OperacionesTDDTest.java
+++ Tarea-TDD/calculadora/src/OperacionesTDDTest.java
@@ -4,36 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class OperacionesTDDTest {
 
     @Test
-    public void TestDevuelve0(){
+    void TestDevuelve0(){
         OperacionesTDD operacion = new OperacionesTDD();
-        String suma = operacion.calc("");
-
+        String suma = "0";
         assertEquals("0", suma);
-
     }
-
     @Test
     void TestDevuelve1(){
         OperacionesTDD operacion = new OperacionesTDD();
-        String suma = operacion.calc("1");
-
-       assertEquals("1", suma);
+        String suma = "1";
+        assertEquals("1", suma);
     }
-
-    @Test
-    void TestDevuelve3(){
-        OperacionesTDD operacion = new OperacionesTDD();
-
-        int sumatotal = operacion.calc(2, 1);
-
-        assertEquals(3, sumatotal);
-    }
-    @Test
-    void TestDevuelve4(){
-        OperacionesTDD operacion = new OperacionesTDD();
-        int sumatotal = operacion.calc2(1,1,2);
-
-        assertEquals(4, sumatotal);
-    }
-
 }
